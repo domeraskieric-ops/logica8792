3324ab26e560d8b697bdd1cad2cd6154bbde9b29
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-09 20:31:34 -0300

    subtração

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main (){
    int a = 5;
    int b = 4;
 
-   printf("O maior número é: %d\n", (a - b));
    printf("O soma dos números é: %d\n", (a + b));
+   printf("A subtração dos números é: %d\n", (a - b));
 
 
 
