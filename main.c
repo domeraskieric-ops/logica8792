8c685a26f6e547db31bf3027cad1b9915a940b83
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-09 20:27:43 -0300

    soma

--- main.c
+++ main.c
@@ -5,7 +5,10 @@ int main (){
    int a = 5;
    int b = 4;
 
-   printf("O maior número é: %d\n", (a > b) ? a : b);
+   printf("O maior número é: %d\n", (a - b));
+   printf("O soma dos números é: %d\n", (a + b));
+
+
 
    return 0;
 }
