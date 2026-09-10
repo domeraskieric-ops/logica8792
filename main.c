51eacb2a03a0f859a123c297d8dcb9dbf4782762
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-09 21:32:49 -0300

    Acento

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
+#include<locale.h>
+
 
 int main (){
+
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
 
    int a = 5;
    int b = 4;
 
-   printf("O soma dos números é: %d\n", (a + b));
+   printf("A soma dos números é: %d\n", (a + b));
    printf("A subtração dos números é: %d\n", (a - b));
    printf("A divisão é: %d\n", (a / b));
    printf("A multiplicção é: %d\n", (a * b));
