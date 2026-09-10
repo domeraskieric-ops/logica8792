dd2c8fa58772ba4d70dfe64327b7dcfdcac31983
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-09 21:54:56 -0300

    ehPar

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
 #include<locale.h>
+#include<stdbool.h>
 
+bool ehPar(int numero){
+    return(numero % 2 == 0);
+}
 
 int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
+    bool resultado1 = ehPar(4);
+    bool resultado2 = ehPar(7);
+    bool resultado3 = ehPar(9);
+    bool resultado4 = ehPar(2);
+    bool resultado5 = ehPar(6);
 
-   int a = 5;
-   int b = 4;
 
-   printf("A soma dos números é: %d\n", (a + b));
-   printf("A subtração dos números é: %d\n", (a - b));
-   printf("A divisão é: %d\n", (a / b));
-   printf("A multiplicção é: %d\n", (a * b));
+    printf("4 é par? %d\n", resultado1);
+    printf("7 é par? %d\n", resultado2);
+    printf("9 é par? %d\n", resultado3);
+    printf("2 é par? %d\n", resultado4);
+    printf("6 é par? %d\n", resultado5);
+
+
+
 
 
 
