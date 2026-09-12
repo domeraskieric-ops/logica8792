76e0b091f36f11df7aaf3efbfb6697d44a6a3b86
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-11 21:49:53 -0300

    else/if

--- main.c
+++ main.c
@@ -5,17 +5,21 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int segundos, horas, minutos, resto;
+   int n;
 
-   printf("digite a quantidade de segundos:");
-   scanf("%d", &segundos);
+   printf("digite um numero:");
+   scanf("%d", &n);
 
-   horas = segundos / 3600;
-   minutos = (segundos % 3600) / 60;
-   resto = segundos % 60;
+   //if(.....){
+   // code....
+   //}
 
-   printf("%d horas, %d minutos e %d segundos\n", horas, minutos, resto);
-
+   if(n > 0){
+    printf("positivo\n");
+   }else{
+    printf("negativo:\n");
+   }
+   
 
    return 0;
 }
