9c3bbf651e987d6db64738a1e311921fbc59e27d
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-14 20:11:31 -0300

    idades

--- main.c
+++ main.c
@@ -7,19 +7,20 @@ int main (){
 
    int idade;
 
-   printf("digite um numero:");
+   printf("digite uma idade:");
    scanf("%d", &idade);
 
-    if (idade >= 18){
-        printf("voce é maior de idade!");
-    }else{
-        printf("voce é maior de idade!");
+   if (idade >= 50){
+    printf("senior");
+   }else if(idade >= 18)
+   {printf("maior de idade");
+   }else if(idade >= 12)
+   {printf("adolescente");
+    }else if (idade >= 9)
+    {printf("PRE-adolescente");
+    }else
+    {printf("crianca");
     }
 
-
-    
-   
-   
-
-   return 0;
+    return 0;
 }
