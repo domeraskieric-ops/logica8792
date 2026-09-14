52b8a4c62f4dabf1ab892bc100d3c8c7f081a662
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-14 20:51:18 -0300

    maior 5 variaveis

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int a,b,c, maior;
+   int a,b,c,d,e,maior;
 
-   printf("digite tres números:");
-   scanf("%d,%d,%d", &a, &b, &c);
+   printf("digite cinco números:");
+   scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
 
    maior = a;
 
    if(b > maior) maior = b;
    if(c > maior) maior = c;
+   if(d > maior) maior = d;
+   if(e > maior) maior = e;
 
    printf("maior: %d", maior);
 
