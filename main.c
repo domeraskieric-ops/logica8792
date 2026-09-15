f99adca9c146c3334f689abd80f8cc82f79c8c34
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-14 21:42:26 -0300

    diferente/iguais

--- main.c
+++ main.c
@@ -5,41 +5,16 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int a,b,c,d,e,menor,maior;
+   int a,b;
 
-    printf("digite um numero para a:");
-    scanf("%d", &a);
+   printf("digite dois numeros:");
+   scanf("%d %d", &a, &b);
 
-    printf("digite um numero para b:");
-    scanf("%d", &b);
-
-    printf("digite um numero para c:");
-    scanf("%d", &c);
-
-    printf("digite um numero para d:");
-    scanf("%d", &d);
-
-    printf("digite um numero para e:");
-    scanf("%d", &e);
-
-   menor = a;
-
-   if(b < menor) menor = b;
-   if(c < menor) menor = c;
-   if(d < menor) menor = d;
-   if(e < menor) menor = e;
-
-   maior = a;
-
-   if(b > maior) maior = b;
-   if(c > maior) maior = c;
-   if(d > maior) maior = d;
-    if(e > maior) maior = e;
-
-
-   printf("menor: %d", menor);
-   
-   printf("maior: %d", maior);
+   if(a == b){
+    printf("os numeros sao iguais!\n");
+   }else{
+    printf("os numeros sao diferentes!\n");
+   }
 
     return 0;
 }
