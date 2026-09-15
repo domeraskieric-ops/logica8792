cabde8d140b0afef5aa2cf9ef4229221dceda66f
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-14 21:59:26 -0300

    tres diferentes

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int a,b;
+   int a,b,c;
 
    printf("digite dois numeros:");
-   scanf("%d %d", &a, &b);
+   scanf("%d %d %d", &a, &b, &c);
 
-   if(a != b){
-    printf("os numeros sao diferentes!\n");
+   if((a != b) && (b !=c)){
+    printf("os tres numeros sao diferentes!\n");
+   }else if(b != c){
+    printf("os dois numeros sao diferentes!\n");
    }else{
-    printf("os numeros sao iguais!\n");
+    printf("todos os numeros sao iguais");
    }
 
     return 0;
