6124d978840ef002ec460f94c2f944d74521c640
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-14 21:49:16 -0300

    iguais/diferentes

--- main.c
+++ main.c
@@ -10,10 +10,10 @@ int main (){
    printf("digite dois numeros:");
    scanf("%d %d", &a, &b);
 
-   if(a == b){
-    printf("os numeros sao iguais!\n");
-   }else{
+   if(a != b){
     printf("os numeros sao diferentes!\n");
+   }else{
+    printf("os numeros sao iguais!\n");
    }
 
     return 0;
