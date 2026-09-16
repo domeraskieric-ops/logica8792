0c1c307a9cf783de1450115de731d1725fe317e8
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-16 19:28:23 -0300

    roleta da morte2.1

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main (){
    printf("\nQual sua altura?");
    scanf("%f", &altura);
 
-   if((idade >= 18) && (altura >= 1.60)){
+   if((idade >= 18) || (altura >= 1.60)){
     printf("Voce pode brincar na roleta!");
    }else{
     printf("Voce nao pode brincar na roleta!");
