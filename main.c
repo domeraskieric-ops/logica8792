e34daacd1067f779e62166d6bc3392fdd661f271
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-16 20:49:39 -0300

    dia da semana2.0

--- main.c
+++ main.c
@@ -10,23 +10,33 @@ int main (){
     printf("Digite um número de (1 a 7):");
     scanf("%d", &diaDaSemana);
 
-    if(diaDaSemana == 1){
-        printf("domingo!");
-    }else if(diaDaSemana == 2){
-        printf("segunda-feira");
-    }else if(diaDaSemana == 3){
-        printf("terça-feira");
-    }else if(diaDaSemana == 4){
-        printf("quarta-feira");
-    }else if(diaDaSemana == 5){
-        printf("quinta-feira");
-    }else if(diaDaSemana == 6){
-        printf("sexta-feira");
-    }else if(diaDaSemana == 7){
-        printf("sabado");
-    }else{
-        printf("Valor invalido");
-    }
+   switch (diaDaSemana){
+        case 1:
+            printf("domingo!");
+            break;
+        case 2:
+            printf("segunda-feira!");
+            break;
+        case 3:
+            printf("terça-feira!");
+            break;
+        case 4:
+            printf("quarta-feira!");
+            break;
+        case 5:
+            printf("quinta-feira!");
+            break;
+        case 6:
+            printf("sexta-feira!");
+            break;
+        case 7:
+            printf("sábado!");
+            break;    
+        default:
+            printf("Número inválido");
+            break;
+   }
+
 
     return 0;
 }
