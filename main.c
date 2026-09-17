dd19ebb2077dfec6d89f962348dac324860b818c
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-16 21:33:02 -0300

    notas escolares

--- main.c
+++ main.c
@@ -5,38 +5,26 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int diaDaSemana;
+    float n1, n2, n3, media;
 
-    printf("Digite um número de (1 a 7):");
-    scanf("%d", &diaDaSemana);
+    printf("digite uma nota 1:");
+    scanf("%f", &n1);
 
-   switch (diaDaSemana){
-        case 1:
-            printf("domingo!");
-            break;
-        case 2:
-            printf("segunda-feira!");
-            break;
-        case 3:
-            printf("terça-feira!");
-            break;
-        case 4:
-            printf("quarta-feira!");
-            break;
-        case 5:
-            printf("quinta-feira!");
-            break;
-        case 6:
-            printf("sexta-feira!");
-            break;
-        case 7:
-            printf("sábado!");
-            break;    
-        default:
-            printf("Número inválido");
-            break;
-   }
+    printf("digite uma nota 2:");
+    scanf("%f", &n2);
 
+    printf("digite uma nota 3:");
+    scanf("%f", &n3);
 
+    media = (n1 + n2 + n3) / 3;
+
+    if(media >= 7){
+        printf("aprovado");
+    }else if(media >= 5){
+        printf("recuperação");
+     }else{
+         printf("reprovado");
+     }
+    
     return 0;
 }
