fd95a6d794ec4e6407763ff1ed8a3fa3febf73ee
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-18 19:17:51 -0300

    pedagio 2.0

--- main.c
+++ main.c
@@ -10,15 +10,23 @@ int main (){
    printf("---- sistema de pedagio -----\n");
    printf("1 - moto\n");
    printf("2 - carro passeio\n");
+   printf("3 - caminhão\n");
+   printf("4 - onibus\n");
    scanf("%d", &opcao);
 
    switch (opcao){
     case 1 :
-        printf("moto | tarifa: R$ 5,00\n");
+        printf("Moto | tarifa: R$ 5,00\n");
         break;
     case 2 :
-        printf("carro paseio R$ 10,00\n");  
+        printf("Carro paseio | R$ 10,00\n");  
         break;
+    case 3 :
+        printf("Caminhão | R$ 25,00\n");
+        break;
+    case 4 :
+        printf("Onibus | R$ 30,00\n");
+        break;   
     default:
         printf("ERRO\n");
         break;      
