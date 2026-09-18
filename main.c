b79a082177fe58ba58c5673ab79d72ae15a0020d
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-18 20:55:13 -0300

    LETRAS

--- main.c
+++ main.c
@@ -5,38 +5,16 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int m;
+    char letra;
+    printf("Digite uma letra:");
+    scanf("%c", &letra);
 
-    printf("digite um numero de 1 a 12 referente ao mes\n");
-    scanf("%d", &m );
-
-    switch (m)
-    {
-    case 12:
-    case 1:
-    case 2:
-        printf("verão\n");
-        break;
-    case 3:
-    case 4:
-    case 5:
-        printf("Outono\n");
-        break;
-    case 6:
-    case 7:
-    case 8:
-        printf("inverno\n");
-        break;
-    case 9:    
-    case 10:
-    case 11:
-        printf("primavera\n");
-        break;
-
-    default:
-        printf("numero invalido\n");
-        break;
-    }
+    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+        letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
+            printf("Vogal\n");     
+        }else{
+            printf("Consoante\n");
+        }
 
     return 0;
 }
