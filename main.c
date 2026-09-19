c5072a5dd8d3c66f80cfa450b3e1223a5e39227a
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-18 21:45:13 -0300

    ARRUMAR

--- main.c
+++ main.c
@@ -5,17 +5,26 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int ano;
-   printf("Digite o ano:");
-   scanf("%d", &ano);
+   float peso, altura, imc;
 
-   if((ano % 4 == 0 && ano % 100 != 0)|| ano % 400 == 0){
-    printf("Ano: %d\n", ano);
-    printf("Ano Bissexto!\n");
-     }else{
-        printf("Ano: %d\n", ano);
-        printf("Ano NÃO Bissexto!\n");
-     }
+   printf("Digite o peso (kg):");
+   scanf("%f", &peso);
 
+   printf("Digite a altura (m)");
+   scanf("%f", &altura);
+
+   imc = peso / (altura * altura);
+
+   if(imc < 18.5){
+    printf("Seu peso é: %f\n", peso);
+    printf("Sua altura é: %f\n, altura");
+    printf("Classificação: Abaixo do peso!\n"); 
+   }else if(imc < 25){
+    printf("Classificação: Peso normal!\n");
+   }else if(imc < 30){
+    printf("Classificação; Sobrepeso!\n");
+   }else{
+    printf("Clssificação: Obesidade!\n");
+   }
     return 0;
 }
