70d88716ad14023e9ff42bcc5ad7689155387b59
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-18 21:17:52 -0300

    ANO

--- main.c
+++ main.c
@@ -5,18 +5,17 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   int n;
-   printf("Digite um número: ");
-   scanf("%d", &n);
+   int ano;
+   printf("Digite o ano:");
+   scanf("%d", &ano);
 
-   if(n % 3 == 0 && n % 5 == 0){
-        printf("multiplo de 3 a 5\n");
-   }else if(n % 3 == 0){
-        printf("multiplo de 3\n");
-    }else if(n % 5 == 0){
-        printf("multiplo de 5\n");
-    }else{
-        printf("não é múltiplo de 3 e nem de 5\n");
-    }
+   if((ano % 4 == 0 && ano % 100 != 0)|| ano % 400 == 0){
+    printf("Ano: %d\n", ano);
+    printf("Ano Bissexto!\n");
+     }else{
+        printf("Ano: %d\n", ano);
+        printf("Ano NÃO Bissexto!\n");
+     }
+
     return 0;
 }
