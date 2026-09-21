461e6c32ffcdbbd0aec2764a9c4788b74fe1b99e
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 19:55:46 -0300

    Char

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    for(int i = 0; i < 1000; i++){
-        printf("%s\n %d", "\nOs números estão aumentando: ---->>" ,i);
-    }
-    printf("\nMeu for funcionou!!");
+    char frutas[3][20] = {"Maça", "Banana", "Laranja"};
+    
+    printf("%s\n", frutas[0]);
+    printf("%s\n", frutas[1]);
+    printf("%s\n", frutas[2]);
+
+
+
     return 0;
 }
