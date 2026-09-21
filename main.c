7a819dfa459b1c45204ca522a6179de6ecc8ee40
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 19:40:40 -0300

    FOR

--- main.c
+++ main.c
@@ -5,26 +5,9 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   float peso, altura, imc;
-
-   printf("Digite o peso (kg):");
-   scanf("%f", &peso);
-
-   printf("Digite a altura (m)");
-   scanf("%f", &altura);
-
-   imc = peso / (altura * altura);
-
-   if(imc < 18.5){
-    printf("Seu peso é: %f\n", peso);
-    printf("Sua altura é: %f\n, altura");
-    printf("Classificação: Abaixo do peso!\n"); 
-   }else if(imc < 25){
-    printf("Classificação: Peso normal!\n");
-   }else if(imc < 30){
-    printf("Classificação; Sobrepeso!\n");
-   }else{
-    printf("Clssificação: Obesidade!\n");
-   }
+    for(int i = 0; i < 10000; i++){
+        printf("%d\n", i);
+    }
+    printf("\nMeu for funcionou!!");
     return 0;
 }
