2531958ca86d103a955c36f343d7c0e698868930
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 20:55:54 -0300

    arrumar e entender

--- main.c
+++ main.c
@@ -5,11 +5,16 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    char nomes[3][20] = {"Jubileu", "Jorge", "Otávio"};
-    
-    printf("%s\n", nomes[0]);
-    printf("%s\n", nomes[1]);
-    printf("%s\n", nomes[2]);
+    int numerosInteiros[5] = {1, 5, 10, 15, 20};
+    float numerosFloat[5] = {10.5, 20.8, 30.2, 40.9, 50.1};
+    char carros[3][20] = {"Ferrari", "Mercedez", "Mazda"};
+
+    printf("%d %d %d %d %d", numerosInteiros[0], numerosInteiros[1], numerosInteiros[2], numerosInteiros[3], numerosInteiros[4]);
+
+    {for (int i = 0; i > 5; i++);}
+    {
+        printf("%.1f\n", numerosFloat);
+    }
 
 
 
