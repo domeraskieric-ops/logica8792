0400e49f0dc4a48f0a0c883ba6bc4cd5c643783c
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 21:02:17 -0300

    idades

--- main.c
+++ main.c
@@ -5,18 +5,13 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int numerosInteiros[5] = {1, 5, 10, 15, 20};
-    float numerosFloat[5] = {10.5, 20.8, 30.2, 40.9, 50.1};
-    char carros[3][20] = {"Ferrari", "Mercedez", "Mazda"};
+    int idades[3] = {25, 35, 45};
 
-    printf("%d %d %d %d %d", numerosInteiros[0], numerosInteiros[1], numerosInteiros[2], numerosInteiros[3], numerosInteiros[4]);
+    printf("%s %d %s", "Manoel tem,", idades[0], "anos!\n");
 
-    {for (int i = 0; i > 5; i++);}
-    {
-        printf("%.1f\n", numerosFloat);
-    }
+    printf("%s %d %s", "Otávio tem,", idades[1], "anos!\n");
 
-
+    printf("%s %d %s", "Duda tem,", idades[2], "anos!\n");
 
     return 0;
 }
