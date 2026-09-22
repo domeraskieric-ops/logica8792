23f3c367c00c7845315a3e490ab1f9b6f1d493dd
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 21:46:32 -0300

    alteração de array

--- main.c
+++ main.c
@@ -5,11 +5,13 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int numeros[] = {1, 2, 3};
-
+    int numeros[3];
+    
     numeros[0] = 10;
+    numeros[1] = 20;
+    numeros[2] = 30;
 
-    printf("%d", numeros[0]);
+    printf("%d", numeros[2]);
 
   
 
