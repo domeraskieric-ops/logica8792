3d80eeacf96c2dab5089ec6ef9f85fdfeae46df5
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 21:30:21 -0300

    soma for

--- main.c
+++ main.c
@@ -5,9 +5,18 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    for(int i = 1000; i >= 0; i = i - 2){
-        printf("%d\n", i);
-    }
+   int soma = 0;
+   int i;
+
+   for(i = 1; i <= 5; i++){
+    //0+1
+    //1+2
+    //3+3
+    //6+4
+    //10+5
+    soma = soma + i;
+   }
+   printf("Soma é %d", soma);
 
     return 0;
 }
