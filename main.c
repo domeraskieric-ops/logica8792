7d54df8f33bc60fea99ba0887bd85180eaccd2de
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-21 21:54:34 -0300

    sizeof

--- main.c
+++ main.c
@@ -5,13 +5,9 @@ int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int numeros[3];
-    
-    numeros[0] = 10;
-    numeros[1] = 20;
-    numeros[2] = 30;
+    int numeros[] = {10, 20, 30};
 
-    printf("%d\n%d\n%d", numeros[2], numeros[1], numeros[0]);
+    printf("%zu", sizeof(numeros));
 
   
 
