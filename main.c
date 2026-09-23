26ec809039488b669cc864c97411f52c9c0040b1
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-22 21:46:22 -0300

    math.h/matematica

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include<stdio.h>
 #include<locale.h>
-
+#include<math.h>
    
 int main (){
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    char nomes[3][20] ={
-        "Eric",
-        "Otavio",
-        "Andressa"
-    }; 
-    for(int i = 0; i < 3; i++){
-        printf("%s\n", nomes[i]);
-    }
-    
-    
-    
+    int numero;
+    printf("Digite um número:");
+    scanf("%d", &numero);
 
+    int quadrado = (int)pow(numero, 2);
+    int cubo = (int)pow(numero, 3);
+    printf("Quadrado de %d = %d\n", numero, quadrado);
+    printf("Cubo de %d = %d", numero, cubo);
 
     return 0;
 }
