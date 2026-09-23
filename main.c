1c2d9d73a2c20d89dab3938063e8f0fa82b18045
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-22 21:13:07 -0300

    for22/09

--- main.c
+++ main.c
@@ -1,48 +1,19 @@
 #include<stdio.h>
 #include<locale.h>
 
-    int somar(int a, int b){
-        return a + b;
-    }
-
-    int subtrair(int a, int b){
-        return a - b;
-    }
-
-    int multiplicador(int a, int b){
-        return a * b;
-    }
-
-    int dividir(int a, int b){
-        if(b <= 0){
-            printf("Ele precisa ser maior que 0!");
-        }else{
-            return a / b;
-        }
-    }
-
-    // void soma(){
-    //     int num1, num2;
-    //     printf("Digite um valor para num1:");
-    //     scanf("%d", &num1);
-    //     printf("Digite um valor para num2:");
-    //     scanf("%d", &num2);
-    //     int resultado = num1 + num2;
-    //     printf("O resultado da soma é %d\n", resultado1 );
-    //}
+   
 int main (){
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int resultado = somar(5, 3);
-    int resultado1 = subtrair(10, 2);
-    int resultado2 = multiplicador(4, 2);
-    int resultado3 = dividir(16, 2);
+    int num[5] = {1, 2, 3, 4, 5};
+
+    for(int i = 0; i < 5; i++){
+        printf("%d\n", num[i]);
+    }
+
     
-    printf("A soma é: %d\n", resultado);
-    printf("A subtração é: %d\n", resultado1);
-    printf("A multiplicação é: %d\n", resultado2);
-    printf("A divisão é: %d\n", resultado3);
-    
+
+
     return 0;
 }
