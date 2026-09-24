d05dcb9f6b7df7d17b6df658f20ee9f346d9d519
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-23 21:41:49 -0300

    MaxMin

--- main.c
+++ main.c
@@ -2,28 +2,22 @@
 #include<locale.h>
 #include<string.h>
 
-    void contarVogais(char palavras[]){
-        int contador = 0;
-        for(int i = 0; i <strlen(palavras); i++){
-            char c = palavras[i];
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-                contador++;
-            }//strlen calcula os caracteres de uma palavra.
+    void maxMin(int v[], int tamanho){
+        int max = v[0], min = v[0];
+        for(int i = 1; i < tamanho; i++){
+            if(v[i] > max) max = v[i];
+            if(v[i] < min) min = v[1];
         }
-        printf("A palavra '%s' tem %d vogais.\n", palavras, contador);
+        printf("Máximo: %d, Mínimo: %d\n", max,min);
     }
     
 int main (){
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    char p[50];
+    int numeros[] = {4, 8, 2, 15, 6};
+    maxMin(numeros, 5);
 
-    printf("Digite uma palavra:\n");
-    scanf("%s", p);
-
-    contarVogais(p);
 
 
     return 0;
