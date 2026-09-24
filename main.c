b8a32d15968d60ebb378d11b8b320b92fc0a956c
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-23 21:54:21 -0300

    EhparImpar

--- main.c
+++ main.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
 #include<locale.h>
 #include<string.h>
-
-    void maxMin(int v[], int tamanho){
-        int max = v[0], min = v[0];
-        for(int i = 1; i < tamanho; i++){
-            if(v[i] > max) max = v[i];
-            if(v[i] < min) min = v[1];
-        }
-        printf("Máximo: %d, Mínimo: %d\n", max,min);
-    }
     
 int main (){
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int numeros[] = {4, 8, 2, 15, 6};
-    maxMin(numeros, 5);
+    int EhparImpar;
 
+    printf("numero");
+    scanf("%d", &EhparImpar);
 
+    if (EhparImpar % 2 == 0){
+        printf("numero par!\n");
+    }else{
+        printf("numero impar");
+    }
 
     return 0;
 }
