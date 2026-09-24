656ccc53d03e1a87887a32c6cd2d0d400de0eaf2
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-23 21:04:12 -0300

    tamanho*****

--- main.c
+++ main.c
@@ -2,21 +2,17 @@
 #include<locale.h>
 #include<math.h>
 
-    void verificarEhParImpar(int n){
-        if(n % 2 ==0){
-            printf("%d é par!\n", n);
-        }else{
-            printf("%d é impar!\n", n);
+    void linha (int tamanho){
+        for(int i = 0; i < tamanho; i++){
+            printf("*");
         }
+        printf("\n");
     }
-
 int main (){
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    verificarEhParImpar(10);
-    verificarEhParImpar(7);
-    
+    linha(10000);
 
     return 0;
 }
