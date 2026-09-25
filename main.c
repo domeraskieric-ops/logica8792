341ab5f6bcee567629aa5c765620222af648d623
ericdomeraski7 <domeraskieric@gmail.com>
2026-09-25 20:58:43 -0300

    while/invertido

--- main.c
+++ main.c
@@ -6,22 +6,19 @@ int main (){
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int n, soma = 0;
+    int n, resto, invertido = 0;
+
     printf("Digite um número:");
     scanf("%d", &n);
-
-    for(int i = 1; i < n; i++){
-        if(n % i == 0){
-            soma += i;
-        }
-    }
-    if(soma == n){
-        printf("%d é um número perfeito\n", n);
-    }else{
-        printf("%d Não é um número perfeito\n", n);
-    }
-
     
+    while(n != 0){
+        resto = n % 10;
+        invertido = invertido * 10 + resto;
+        n /= 10;
+    }
+
+    printf("Número invertido: %d\n", invertido);
+
     return 0;
 }
 
